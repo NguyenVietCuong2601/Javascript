11036a0645be2588e13fb6b59d5513a82e107893
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-06-09 15:54:48 +0700

    built-in functions

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,1 +1,17 @@
-alert("Hi javascript")
+var fullName = "Nguyen Viet Cuong";
+var age = 26;
+
+// alert(fullName);
+// alert(age);
+
+// console.log(fullName, age, 'hello')
+// confirm('Xac nhan ban du tuoi')
+// prompt('xac nhan ban du tuoi')
+
+// setTimeout(function() {
+//     alert(age)
+// }, 1000)
+
+// setInterval(function() {
+//     console.log('day la log ' + Math.random())
+// }, 1000);
