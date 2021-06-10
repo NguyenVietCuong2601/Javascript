f66900321297eed0fad76579ba1315851c3417cb
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-06-10 23:22:08 +0700

    prefix & postfix

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -3,3 +3,17 @@ Arithmetic
 + , - , * , / , % , ++ , --
 ** --> Luy thua;
 */
+
+// Toan tu ++ --
+// Prefix & Postfix
+
+/*  VD: a
+    Prefix: ++a
+    - + 1 cho a, a = a+1 
+    - Tra ve a sau khi duoc + 1
+
+    Postfix: a++
+    - copy a
+    - cong 1 cho a, a = a-1
+    - Tra ve copy a
+*/
