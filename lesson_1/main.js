5c924ad43ef15f9ae823f54179697741eb4653a0
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-06-10 22:57:19 +0700

    arithmetic

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,17 +1,5 @@
-var fullName = "Nguyen Viet Cuong";
-var age = 26;
-
-// alert(fullName);
-// alert(age);
-
-// console.log(fullName, age, 'hello')
-// confirm('Xac nhan ban du tuoi')
-// prompt('xac nhan ban du tuoi')
-
-// setTimeout(function() {
-//     alert(age)
-// }, 1000)
-
-// setInterval(function() {
-//     console.log('day la log ' + Math.random())
-// }, 1000);
+/*
+Arithmetic
++ , - , * , / , % , ++ , --
+** --> Luy thua;
+*/
