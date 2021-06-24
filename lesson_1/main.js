81619604d77b02cf9e6ade3ca5913e2c7417be36
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-06-24 23:30:19 +0700

    includes method

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -610,22 +610,33 @@ Arithmetic
 
 //Reduce medthods----------------------------------------------------
 
-Array.prototype.reduce2 = function(callback, result) {
-    let i = 0
-    if (arguments.length < 2) {
-        i = 1;
-        result = this[0];
-    }
-    for (; i < this.length; i++) {
-       result = callback(result, this[i], i, this)
-    }
-    return result;
-}
+// Array.prototype.reduce2 = function(callback, result) {
+//     let i = 0
+//     if (arguments.length < 2) {
+//         i = 1;
+//         result = this[0];
+//     }
+//     for (; i < this.length; i++) {
+//        result = callback(result, this[i], i, this)
+//     }
+//     return result;
+// }
 
-const numbers = [1, 2, 3, 4, 5]
+// const numbers = [1, 2, 3, 4, 5]
 
-const result = numbers.reduce2(function(total, number) {
-    return total + number;
-})
+// const result = numbers.reduce2(function(total, number) {
+//     return total + number;
+// })
 
-console.log(result)
+// console.log(result)
+
+//includes method----------------------------------------------------
+
+var title = 'Responsive web design';
+
+console.log(title.include('web', 1));
+
+var courses = ['JS', 'PHP', 'Dart'];
+
+// neu chi so am thi = length + chi so am --- VD: 3 + -1 = 2
+console.log(courses.includes('JS', -1))
