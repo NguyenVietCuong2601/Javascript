a94b0fa468480369b2dd207808c487cf7b4c7049
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-06-28 23:31:41 +0700

    DOM attributes, innerText, textContent & innerHTML, outerHTML

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -9,7 +9,7 @@
  * 3. Text
  */
 
-// --------------------------------------------
+// -------------------------------------------------------------------------------------------
 
 // Javascript: Browser | Server (NodeJS)
 
@@ -19,28 +19,52 @@
 
 // var headingNodes = document.getElementsByTagName('p');
 
-var headingNode = document.querySelector('.heading')
+// var headingNode = document.querySelector('.heading')
 
-console.log({
-    element: headingNode
-});
-
-// Làm bài tập tại đây
-
-var boxHTMLCollection = document.getElementsByClassName('box');
-var box1Element = document.querySelector('.box:first-child');
-var box2Element = document.querySelector('.container .box:nth-child(2)');
-var allItemElements = document.getElementsByTagName('li');
-var box1ItemElements = box1Element.getElementsByTagName('li');
-var box2ItemElements = box2Element.getElementsByTagName('li');
-
-var h1Element = document.querySelector('h1');
-var h2Element = document.querySelector('h2');
-var h3Element = document.querySelector('h3');
+// console.log({
+//     element: headingNode
+// });
 
 // Làm bài tập tại đây
 
-const checkboxNodes = document.querySelectorAll('input');
-const checkbox1Element = checkboxNodes.querySelector("input[value='1']");
+// var boxHTMLCollection = document.getElementsByClassName('box');
+// var box1Element = document.querySelector('.box:first-child');
+// var box2Element = document.querySelector('.container .box:nth-child(2)');
+// var allItemElements = document.getElementsByTagName('li');
+// var box1ItemElements = box1Element.getElementsByTagName('li');
+// var box2ItemElements = box2Element.getElementsByTagName('li');
 
-console.log(checkboxNodes);
+// var h1Element = document.querySelector('h1');
+// var h2Element = document.querySelector('h2');
+// var h3Element = document.querySelector('h3');
+
+// DOM Attribute -----------------------------------------------------------------------------
+
+var headingElement = document.querySelector('h1');
+
+//headingElement.setAttribute('data', 'test-data');
+
+//console.log(headingElement.getAttribute('class'));
+//console.log(headingElement.getAttribute('data'));
+
+// innerText, textContent ----------------------------------------------------- --------------
+
+var headingElement = document.querySelector('h1.heading');
+
+//headingElement.innerText = 'New heading';
+
+//console.log(headingElement.innerText);
+//console.log(headingElement.textContent);
+
+//console.log(headingElement);
+//headingElement.innerText = '<i>New heading</i>';
+
+// InnerHTML, OuterHTML-----------------------------------------------------------------------
+
+var boxElement = document.querySelector('.box');
+
+//boxElement.innerHTML = '<span>Test</span>';
+//boxElement.outerHTML = '<span>Test</span>'
+
+//console.log(boxElement.outerHTML);
+//console.log(document.querySelector('h1').innerText);
