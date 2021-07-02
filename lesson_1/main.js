9bb68e66228cec8bf33a2c1b6387ad41ee266564
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-07-02 23:32:17 +0700

    Promise chain, resolve, reject, all & example

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -209,32 +209,207 @@
 // 2.Fulfilled
 // 3.Reject
 
-var promise = new Promise(
-    //Excutor
-    function(resolve, reject) {
-        //Logic
-        //Thanh cong: resolve()
-        //That bai: reject()
+// var promise = new Promise(
+//     //Excutor
+//     function(resolve, reject) {
+//         //Logic
+//         //Thanh cong: resolve()
+//         //That bai: reject()
 
-        //Fake call API
-        // resolve([
-        //     {
-        //         id: 1,
-        //         name: 'Javascript'
-        //     }
-        // ]);
+//         //Fake call API
+//         // resolve([
+//         //     {
+//         //         id: 1,
+//         //         name: 'Javascript'
+//         //     }
+//         // ]);
 
-        reject('Ma loi!')
+//         //reject('Ma loi!');
+//         resolve();
+//     }
+// );
+
+// promise chain -----------------------------------------------------------------------------
+//     .then(function() {
+//         return new Promise(function(resolve) {
+//             setTimeout(function() {
+//                 resolve([1,2,3]);
+//             }, 3000);
+//         })
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//         return 2;
+//     })
+//     .catch(function(error) {
+//         console.log(error);
+//     })
+//     .finally(function() {
+//         console.log('Done!');
+//     })
+
+// Promise (resolve, reject, all) ------------------------------------------------------------
+// function sleep(ms) {
+//     return new Promise(function(resolve) {
+//         setTimeout(resolve, ms);
+//     })
+// }
+
+// sleep(1000)
+//     .then(function(){
+//         console.log(1);
+//         return sleep(1000);
+//     })
+//     .then(function(){
+//         console.log(2);
+//         return new Promise(function(resolve, reject) {
+//             reject('Co loi!');
+//         });
+//     })
+//     .then(function(){
+//         console.log(3);
+//         return sleep(1000);
+//     })
+//     .then(function(){
+//         console.log(4);
+//         return sleep(1000);
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+
+// 1, Promise.resolve
+// 2. Promise.reject
+// 3. Promise.all
+
+// var promise = new Promise(function(resolve, reject) {
+//     //resolve('Success!');
+//     reject('Error!')
+// })
+
+//var promise = Promise.resolve('Success!');
+//var promise = Promise.reject('Error!');
+
+// promise
+//     .then(function(result) {
+//         console.log(result);
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+
+// var promise1 = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         resolve([1]);
+//     }, 2000);
+// })
+
+// var promise2 = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         resolve([2, 3]);
+//     }, 5000);
+// })
+
+// var promise3 = Promise.reject('Co loi!');
+
+// Promise.all([promise1, promise2, promise3])
+//     .then(function(result) {
+//         var result1 = result[0];
+//         var result2 = result[1];
+        
+//         console.log(result1.concat(result2));
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+
+// Promise example ---------------------------------------------------------------------------
+
+var users = [
+    {
+        id: 1,
+        name: 'Kien Dam',
+    },
+    {
+        id: 2,
+        name: 'Cuong'
+    },
+    {
+        id: 3,
+        name: 'Son Dang'
     }
-);
+    //...
+];
 
-promise
-    .then(function(courses) {
-        console.log(courses);
+var comments = [
+    {
+        id: 1,
+        user_id: 1,
+        content: 'abcdzxawd :))'
+    },
+    {
+        id: 2,
+        user_id: 2,
+        content: 'hi hi :))'
+    },
+    {
+        id: 3,
+        user_id: 3,
+        content: 'OK em :3'
+    }
+];
+
+// Fake API
+// 1. Array
+// 2. Function
+// 3. Promise
+// 4. DOM
+
+function getComments() {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve(comments);
+        }, 1000)
     })
-    .catch(function(error) {
-        console.log(error);
+};
+
+function getUsersByIds(userIds) {
+    return new Promise(function(resolve) {
+        var result = users.filter(function(user) {
+            return userIds.includes(user.id);
+        })
+        setTimeout(function() {
+            resolve(result);
+        }, 1000)
     })
-    .finally(function() {
-        console.log('Done!');
+}
+
+getComments()
+    .then(function(comments) {
+        var userIds = comments.map(function(comment) {
+           return comment.user_id;
+       })
+
+        return getUsersByIds(userIds)
+            .then(function(users) {
+                return {
+                    users: users,
+                    comments: comments
+                }
+            })
+    })
+    .then(function(data) {
+        
+        var commentBlock = document.getElementById('comment-block');
+
+        var html = '';
+
+        data.comments.forEach(function(comment) {
+            var user = data.users.find(function(user) {
+                return user.id === comment.user_id;
+            })
+            html += `<li>${user.name}: ${comment.content}</li>`;
+        });
+
+        commentBlock.innerHTML = html;
     })
