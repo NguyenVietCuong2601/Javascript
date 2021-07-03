d59001de85d8d0c49e3feea3824ba1053601ed7a
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-07-03 23:27:02 +0700

    fecth API & json-server

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -325,39 +325,39 @@
 
 // Promise example ---------------------------------------------------------------------------
 
-var users = [
-    {
-        id: 1,
-        name: 'Kien Dam',
-    },
-    {
-        id: 2,
-        name: 'Cuong'
-    },
-    {
-        id: 3,
-        name: 'Son Dang'
-    }
-    //...
-];
+// var users = [
+//     {
+//         id: 1,
+//         name: 'Kien Dam',
+//     },
+//     {
+//         id: 2,
+//         name: 'Cuong'
+//     },
+//     {
+//         id: 3,
+//         name: 'Son Dang'
+//     }
+//     //...
+// ];
 
-var comments = [
-    {
-        id: 1,
-        user_id: 1,
-        content: 'abcdzxawd :))'
-    },
-    {
-        id: 2,
-        user_id: 2,
-        content: 'hi hi :))'
-    },
-    {
-        id: 3,
-        user_id: 3,
-        content: 'OK em :3'
-    }
-];
+// var comments = [
+//     {
+//         id: 1,
+//         user_id: 1,
+//         content: 'abcdzxawd :))'
+//     },
+//     {
+//         id: 2,
+//         user_id: 2,
+//         content: 'hi hi :))'
+//     },
+//     {
+//         id: 3,
+//         user_id: 3,
+//         content: 'OK em :3'
+//     }
+// ];
 
 // Fake API
 // 1. Array
@@ -365,51 +365,91 @@ var comments = [
 // 3. Promise
 // 4. DOM
 
-function getComments() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            resolve(comments);
-        }, 1000)
-    })
-};
+// function getComments() {
+//     return new Promise(function(resolve) {
+//         setTimeout(function() {
+//             resolve(comments);
+//         }, 1000)
+//     })
+// };
 
-function getUsersByIds(userIds) {
-    return new Promise(function(resolve) {
-        var result = users.filter(function(user) {
-            return userIds.includes(user.id);
-        })
-        setTimeout(function() {
-            resolve(result);
-        }, 1000)
-    })
-}
+// function getUsersByIds(userIds) {
+//     return new Promise(function(resolve) {
+//         var result = users.filter(function(user) {
+//             return userIds.includes(user.id);
+//         })
+//         setTimeout(function() {
+//             resolve(result);
+//         }, 1000)
+//     })
+// }
 
-getComments()
-    .then(function(comments) {
-        var userIds = comments.map(function(comment) {
-           return comment.user_id;
-       })
+// getComments()
+//     .then(function(comments) {
+//         var userIds = comments.map(function(comment) {
+//            return comment.user_id;
+//        })
 
-        return getUsersByIds(userIds)
-            .then(function(users) {
-                return {
-                    users: users,
-                    comments: comments
-                }
-            })
-    })
-    .then(function(data) {
+//         return getUsersByIds(userIds)
+//             .then(function(users) {
+//                 return {
+//                     users: users,
+//                     comments: comments
+//                 }
+//             })
+//     })
+//     .then(function(data) {
         
-        var commentBlock = document.getElementById('comment-block');
+//         var commentBlock = document.getElementById('comment-block');
 
-        var html = '';
+//         var html = '';
 
-        data.comments.forEach(function(comment) {
-            var user = data.users.find(function(user) {
-                return user.id === comment.user_id;
-            })
-            html += `<li>${user.name}: ${comment.content}</li>`;
-        });
+//         data.comments.forEach(function(comment) {
+//             var user = data.users.find(function(user) {
+//                 return user.id === comment.user_id;
+//             })
+//             html += `<li>${user.name}: ${comment.content}</li>`;
+//         });
 
-        commentBlock.innerHTML = html;
+//         commentBlock.innerHTML = html;
+//     })
+
+// Fetch -------------------------------------------------------------------------------------
+// API (URL) -> Application programing interface
+// Cong giao tiep giua cac PM
+// Backend -> API (URL) -> Fetch -> JSON/XML
+// -> JSON.parse -> JS types
+// -> Render ra giao dien voi HTML
+
+// var postApi = 'https://jsonplaceholder.typicode.com/posts';
+
+//     //stream
+// fetch(postApi)
+//     .then(function(response) {
+//         return response.json(); // JSON.parse
+//     })
+//     .then(function(posts) {
+//         var htmls = posts.map(function(post) {
+//             return `<li>
+//                 <h2>${post.title}</h2>
+//                 <p>${post.body}</p>
+//             </li>`;
+//         });
+
+//         var html = htmls.join('');
+//         document.getElementById('post-block').innerHTML = html;
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+
+// JSON server: API server -------------------------------------------------------------------
+var courseApi = 'http://localhost:3000/courses'
+
+fetch(courseApi)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(courses) {
+        console.log(courses);
     })
