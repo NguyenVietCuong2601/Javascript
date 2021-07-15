104d793f8115dd503d3e13ab4b0e8e9a98887138
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-07-15 23:08:16 +0700

    from validation2-way1 final

--- js_demo_02/validator.js
+++ js_demo_02/validator.js
@@ -2,11 +2,20 @@
 // Doi tuong 'Validator'
 function Validator(options) {
 
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    }
+
     var selectorRules = {};
 
     // Ham thuc hien validate
     function validate(inputElement, rule) {
-        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
         var errorMessage;
 
         // Lay ra cac rules cua selector
@@ -15,16 +24,25 @@ function Validator(options) {
         // Lap qua tung rule & kiem tra
         // Neu co loi thi dung viec kiem tra
         for (var i = 0; i < rules.length; i++) {
-            errorMessage = rules[i](inputElement.value);
+            switch (inputElement.type) {
+                case 'radio':
+                case 'checkbox':
+                    errorMessage = rules[i](
+                        formElement.querySelector(rule.selector + ':checked')
+                    );
+                    break;
+                default:
+                    errorMessage = rules[i](inputElement.value);
+            }
             if (errorMessage) break;
         }
                     
         if (errorMessage) {
             errorElement.innerText = errorMessage;
-            inputElement.parentElement.classList.add('invalid');
+            getParent(inputElement, options.formGroupSelector).classList.add('invalid');
         } else {    
             errorElement.innerText = '';
-            inputElement.parentElement.classList.remove('invalid');
+            getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
         }
 
         return !errorMessage;
@@ -42,7 +60,7 @@ function Validator(options) {
             // Lap qua tung rule & validate
             options.rules.forEach(function (rule) {
                 var inputElement = formElement.querySelector(rule.selector);
-                 var isValid = validate(inputElement, rule);
+                var isValid = validate(inputElement, rule);
                 if (!isValid) {
                     isFormValid = false;
                 }
@@ -53,7 +71,27 @@ function Validator(options) {
                 if (typeof options.onSubmit === 'function') {
                     var enableInputs = formElement.querySelectorAll('[name]');
                     var formValues = Array.from(enableInputs).reduce(function (values, input) {
-                        return (values[input.name] = input.value) && values;
+                        switch(input.type) {
+                            case 'radio':
+                                values[input.name] =formElement.querySelector('input[name="' +  input.name + '"]:checked').value;
+                                break;
+                            case 'checkbox':
+                                if (!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values
+                                };
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                        }
+                        return values;
                     }, {});
                     options.onSubmit(formValues);
                 } 
@@ -74,11 +112,11 @@ function Validator(options) {
                 selectorRules[rule.selector] = [rule.test]
             }
 
-            var inputElement = formElement.querySelector(rule.selector);
+            var inputElements = formElement.querySelectorAll(rule.selector);
 
-            if (inputElement) {
-                // XU ly truong hop blur khoi input
-                inputElement.onblur = function () {
+            Array.from(inputElements).forEach(function (inputElement) {
+                 // XU ly truong hop blur khoi input
+                 inputElement.onblur = function () {
                     // value: inputElement.value
                     // test func: rule.test
                     validate(inputElement, rule);
@@ -86,11 +124,11 @@ function Validator(options) {
 
                 // Xu ly truong moi khi nguoi dung nhap vao input
                 inputElement.oninput = function () { 
-                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+                    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
                     errorElement.innerText = '';
-                    inputElement.parentElement.classList.remove('invalid');
+                    getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
                 }
-            }
+            })
         });
 
     }
@@ -104,7 +142,7 @@ Validator.isRequired = function (selector, message) {
     return {
         selector: selector,
         test: function(value) {
-            return value.trim() ? undefined : message || 'Vui long nhap truong nay'
+            return value ? undefined : message || 'Vui long nhap truong nay'
         }
     };
 }
@@ -128,7 +166,7 @@ Validator.minLength = function (selector, min, message) {
     };
 }
 
-Validator.isComfirmed = function (selector, getComfirmValue, message) {
+Validator.isConfirmed = function (selector, getComfirmValue, message) {
     return {
         selector: selector,
         test: function(value) {
