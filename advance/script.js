60add444a3b34b8759607cf15831694a328e94e3
Viet Cuong Nguyen <nguyenvietcuong1999@gmail.com>
2021-08-05 15:40:28 +0700

    Closure

--- advance/script.js
+++ advance/script.js
@@ -110,7 +110,7 @@ const age = 18;
 // When is the var deleted (life cycle)
 {
     const age = 16;
-    console.log(age);
+    //console.log(age);
 }
 
 function logger() {
@@ -130,7 +130,95 @@ function makeCounter() {
 }
 
 const increase1 = makeCounter();
+//console.log(increase1());
+//console.log(increase1());
+//console.log(increase1());
 
-console.log(increase1());
-console.log(increase1());
-console.log(increase1());
+// Closure -----------------------------------------------------------------------------------
+// A function can remember the place where it is created and accesses the var outside its scope
+function createCounter() {
+    let counter = 0;
+    function increase() {
+        return ++counter;
+    }
+    return increase;
+}
+// const counter1 = createCounter();
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+
+function createLogger(namespace) {
+    function logger(message) {
+        console.log(`[${namespace}] ${message}`)
+    }
+
+    return logger;
+}
+ //================== App ===================
+ 
+//  // Register.js
+// const infoLogger = createLogger('Info');
+// infoLogger('Bat dau gui mail');
+// infoLogger('Gui mail loi lan 1, thu gui lai...');
+// infoLogger('Gui mai thanh cong cho user xxx');
+
+// // ForgotPassword.js
+// const errorLogger = createLogger('Error');
+// errorLogger('Email khong ton tai trong DB');
+// errorLogger('Gui mail loi lan 1, thu lai...');
+// errorLogger('Gui mail thanh cong cho user xxx');
+
+function createStorage(key) {
+    const store = JSON.parse(localStorage.getItem(key)) ?? {};
+
+    const save = () => {
+        localStorage.setItem(key, JSON.stringify(store));
+    }
+
+    const storage = {
+        get(key) {
+            return store[key];
+        },
+        set(key, value) {
+            store[key] = value;
+            save();
+        },
+        remove(key) {
+            delete store[key];
+            save();
+        }
+    }
+    return storage;
+}
+// const profileSetting = createStorage('profile_setting');
+// console.log(profileSetting.get('fullName'));
+// profileSetting.set('fullName', 'Cuong Nguyen');
+// profileSetting.set('age', 20);
+// profileSetting.set('address', 'Da Nang');
+
+// const profileSetting2 = createStorage('profile_setting_2');
+// console.log(profileSetting2.get('fullName'));
+// profileSetting2.set('fullName', 'Cuong Nguyen');
+// profileSetting2.set('age', 20);
+// profileSetting2.set('address', 'Da Nang');
+
+function createApp() {
+    const cars = [];
+
+    return {
+        get() {
+            console.log(cars);
+        },
+        add(car) {
+            cars.push(car);
+        },
+        delete(index){
+            cars.splice(index, 1);
+        }
+    }
+}
+// const app1 = createApp();
+// app1.get();
+// app1.add('BMW');
+// app1.get();
